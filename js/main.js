c7628f588a0acab77f9b5f512c938991fab0d28e
mmtrucealt <158788027+mmtrucealt@users.noreply.github.com>
2024-09-27 02:16:39 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // duh
     var adScript = document.createElement('script');
     adScript.async = true;
     adScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3317735030374126";

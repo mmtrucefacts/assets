9020e07f4fb7d3cfbe99323f397b245f7e824a73
mmtrucealt <158788027+mmtrucealt@users.noreply.github.com>
2024-09-27 00:56:37 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var footer = document.createElement('footer');
-    footer.innerHTML = `
-        <div class="footer-content">
-            <span>Illuminate | </span>
-            <a href="https://github.com/xlluminate/Illuminate" target="_blank">GitHub</a>
-        </div>
-    `;
+    var adScript = document.createElement('script');
+    adScript.async = true;
+    adScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3317735030374126";
+    adScript.setAttribute("crossorigin", "anonymous");
 
-    document.body.appendChild(footer);
-
-    var style = document.createElement('style');
-    style.innerHTML = `
-        footer {
-            position: fixed;
-            bottom: 0;
-            width: 100%;
-            background-color: #333;
-            color: white;
-            text-align: center;
-            padding: 10px 0;
-            font-family: Arial, sans-serif;
-            font-size: 14px;
-        }
-
-        .footer-content a {
-            color: lightblue;
-            text-decoration: none;
-            margin-left: 5px;
-        }
-
-        .footer-content a:hover {
-            text-decoration: underline;
-        }
-    `;
-    
-    document.head.appendChild(style);
+    document.head.appendChild(adScript);
 });
 console.log('This file should be linked to every file. If this is not the case, then it is considered a bug.');

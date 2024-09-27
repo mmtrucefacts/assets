4582fe0049b5d1bf89984c5e96e29ef7f546325f
mmtrucealt <158788027+mmtrucealt@users.noreply.github.com>
2024-09-27 11:42:50 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // duh
-    var adScript = document.createElement('script');
-    adScript.async = true;
-    adScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3317735030374126";
-    adScript.setAttribute("crossorigin", "anonymous");
+    var messageDiv = document.createElement('div');
+    messageDiv.innerHTML = `
+        <div class="body-message">
+            <span>Illuminate | </span>
+            <a href="https://github.com/xlluminate/Illuminate" target="_blank">GitHub</a>
+        </div>
+    `;
 
-    document.head.appendChild(adScript);
+    document.body.appendChild(messageDiv);
+
+    // CSS
+    var style = document.createElement('style');
+    style.innerHTML = `
+        .body-message {
+            position: relative;
+            bottom: 0;
+            width: 100%;
+            background-color: #333;
+            color: white;
+            text-align: center;
+            padding: 10px 0;
+            font-family: Arial, sans-serif;
+            font-size: 14px;
+            margin-top: 20px; /* Pushes the content above slightly */
+        }
+
+        .body-message a {
+            color: lightblue;
+            text-decoration: none;
+            margin-left: 5px;
+        }
+
+        .body-message a:hover {
+            text-decoration: underline;
+        }
+    `;
+
+    document.head.appendChild(style);
 });
+
 console.log('This file should be linked to every file. If this is not the case, then it is considered a bug.');
